43220af1b93f5223ce15ae1fe56999e6d391ea60
coffeeandpouch <sameinsidler@me.com>
2020-10-20 12:24:31 -0500

    latest changes

---
+++ src/GoogleBooksExercise/searchBooks.js
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./searchBooks.css";
+
+import "./bookType.js";
+import "./header.js";
+import "./printType.js";
+
+class SearchBooks extends Component {
+  render() {
+    return (
+      <div>
+        <h1>Google Book Search</h1>
+      </div>
+    );
+  }
+}
+export default SearchBooks;
